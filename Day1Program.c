c010ea59ed196264d7bc218d4a7ad8601ea522df
pankhuri007-007 <pankhurikhanna71@gmail.com>
2026-09-02 23:39:58 +0530

    Add files via upload

---
+++ Day1Program.c
@@ -0,0 +1,14 @@
+//(User Inputs, Operations & Output)
+// Q1.Write a program to input two numbers and display their sum.
+#include <stdio.h>
+
+int main() {
+
+    int a = 3, b = 4, sum;
+
+    sum = a + b;
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
