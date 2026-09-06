6c678cff69ae53c95a97308f0701fbcb7513d796
pankhuri007-007 <pankhurikhanna71@gmail.com>
2026-09-06 16:47:31 +0530

    Add files via upload

---
+++ Day3.2.1.c
@@ -0,0 +1,17 @@
+//Q6 (User Inputs, Operations & Output)
+//Write a program to swap two numbers using a third variable.
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 3, b = 5, temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swap: %d %d", a, b);
+
+    return 0;
+}
